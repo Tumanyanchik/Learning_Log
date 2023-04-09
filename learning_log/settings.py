c42e9aae8598a0875a2b47d46ba788e2898d367a
Tumanyanchik <tumanyan2000@yandex.ru>
2023-04-09 19:28:18 +0300

    Add static files for admin panel

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -81,7 +81,6 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -128,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
