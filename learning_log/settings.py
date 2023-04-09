1338fdc842174e1f2ba8a1d1a1ac0d4b7ec8d6ce
Tumanyanchik <tumanyan2000@yandex.ru>
2023-04-09 14:28:51 +0300

    SQL Lite -> PostgreSQL

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LearningLogs',
+        'USER': 'postgres',
+        'PASSWORD': '170420',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
